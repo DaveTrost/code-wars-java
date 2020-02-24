5167d7e68d5c2cafce648dd5dade5fb21845fddd
Dave Trost <davetrost@gmail.com>
2020-02-24 15:39:02 -0800

    first kata in Java: extra perfect numbers

---
+++ java/extra_perfect_numbers/Solution.java
@@ -0,0 +1,20 @@
+package java.extra_perfect_numbers;
+
+import java.util.List;
+import java.util.ArrayList; 
+
+public class Solution
+{
+    public static int[] extraPerfect(int number)
+    {
+        List<Integer> perfects = new ArrayList<Integer>();
+        for (int i = 1; i <= number; i++) {
+          int smallestBit = i & 1;
+          // largest bit is always "set" or "1" (we assume after reading the test input that there is no 'padding' on the bits in the numbers)
+          if (smallestBit == 1) {
+            perfects.add(i);
+          }
+        }
+        return perfects.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
