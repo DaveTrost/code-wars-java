93bc15963a090d55d637a77df7d6126d81df05bf
Dave Trost <davetrost@gmail.com>
2020-02-24 16:41:15 -0800

    java kata for Banker's Plan

---
+++ java/BankerPlan.java
@@ -0,0 +1,17 @@
+package java;
+
+public class BankerPlan {
+    
+    public static boolean fortune(int f0, double p, int c0, int n, double i) {
+        int years = 0;
+        int balance = f0;
+        int neededIncome = c0;
+        while (balance >= 0 && years < n) {
+            balance = (int) (balance * (1 + p / 100) - neededIncome);
+            neededIncome = (int) (neededIncome * (1 + i / 100));
+            years++;
+        }
+        System.out.printf("f0=%d p=%f c0=%d n=%d i=%f result=%b\n", f0, p, c0, n, i, years >= n);
+        return years >= n;
+    }
+}
